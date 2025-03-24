c0bb976e34ac5054441c454ca088b513961d2fc3
Terje Moe <terjemoe54@hotmail.com>
2025-03-24 12:02:27 +0100

    Finish Chapter 20

--- Contacts App/Views/Main Views/ContactListView.swift
+++ Contacts App/Views/Main Views/ContactListView.swift
@@ -31,9 +31,12 @@ struct ContactListView: View {
         order: .forward
     )
     private var contactsByPhoneNumber: [Contact]
-     
-  
+    
+    // State variables
     @State private var searchText: String = ""
+    @State private var selectedSortOrder: SortOrder = .firstName
+    @State private var isSortOrderInverse: Bool = false
+    @State private var isAdvancedShown: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -41,10 +44,35 @@ struct ContactListView: View {
                 TextField("Search Contacts", text: $searchText)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
-              
+                
+                
+                
+                SortAndToggleView(
+                    selectedSortOrder: $selectedSortOrder,
+                    isSortOrderInverse: $isSortOrderInverse
+                )
+                
+                if isAdvancedShown {
+                    Text("TODO: Filter Picker View")
+                }
+                
+                
                 Spacer()
                 
             }.navigationTitle("Contacts")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            isAdvancedShown.toggle()
+                            if !isAdvancedShown {
+                                selectedSortOrder = .firstName
+                            }
+                        } label: {
+                            Label("Advanced", systemImage: isAdvancedShown ? "wand.and.stars" : "wand.and.stars.inverse")
+                        }
+
+                    }
+                }
         }
     }
 }
