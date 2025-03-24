ca8209b7ed209e83db6cfdf3e795706b48cc35c6
Terje Moe <terjemoe54@hotmail.com>
2025-03-24 07:12:30 +0100

    Initial Commit

---
+++ Contacts App/Contacts_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_AppApp.swift
+//  Contacts App
+//
+//  Created by Terje Moe on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
